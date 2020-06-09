18bd701e73eae551bbed1a3d8d7280ea38a7ffad
amcodin <amichel.tech@gmail.com>
2020-06-09 18:14:01 +1000

    security, validation, moved clarifai to backend

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ import Register from "./components/Register/Register";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
-import "./App.css";
 
-const app = new Clarifai.App({
-  apiKey: "901764eea3e843f59116d81680bdb68b",
-});
+import "./App.css";
 
 const particlesOptions = {
   particles: {
@@ -83,8 +79,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
       .then((response) => {
         if (response) {
           fetch("http://localhost:3000/image", {
